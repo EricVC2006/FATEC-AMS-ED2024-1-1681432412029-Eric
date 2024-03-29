47af9792972b089015e50e7344e1968244f938ec
EricVC2006 <161515492+EricVC2006@users.noreply.github.com>
2024-03-28 22:36:10 -0300

    Add files via upload

---
+++ Estruturas-de-dados-Aula02/Estruturas-de-dados-Aula02/programa1.c
@@ -0,0 +1,64 @@
+/*--------------------------------------------------------------------------*/
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados            */
+/*                         Prof Veríssimo                                   */
+/*             Objetivo: Exercícios envolvendo ponteiros e arrays           */
+/*             Paradigama: Programação Modular                              */
+/*                                                                          */
+/*                                                       Data:18/03/2024    */
+/*--------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+#define TAMANHO_ARRAY 5
+
+void ordenarArray(int *array, int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (*(array + j) > *(array + j + 1)) {
+                int temp = *(array + j);
+                *(array + j) = *(array + j + 1);
+                *(array + j + 1) = temp;
+            }
+        }
+    }
+}
+
+
+int encontrarMaiorElemento(int *array, int tamanho) {
+    int maior = *array;
+    int *ponteiro = array;
+    
+    for (int i = 1; i < tamanho; i++) {
+        if (*(ponteiro + i) > maior) {
+            maior = *(ponteiro + i);
+        }
+    }
+    
+    return maior;
+}
+
+int main() {
+    int array[TAMANHO_ARRAY];
+    
+    printf("Digite os %d elementos do array:\n", TAMANHO_ARRAY);
+    for (int i = 0; i < TAMANHO_ARRAY; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    int maior = encontrarMaiorElemento(array, TAMANHO_ARRAY);
+    
+    printf("O maior elemento do array é: %d\n", maior);
+
+
+     ordenarArray(array, TAMANHO_ARRAY);
+
+    printf("Elementos do array em ordem crescente:\n");
+    for (int i = 0; i < TAMANHO_ARRAY; i++) {
+        printf("%d ", *(array + i));
+    }
+    printf("\n");
+
+    return 0;
+
+}
